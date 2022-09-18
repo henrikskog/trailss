e0d39bad312cd6139f94632ea81c0baba7456f54
Victor Martinez <vicroc.vn@gmail.com>
2022-09-18 13:23:59 +0200

    db_auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb+srv://admin:${process.env.DB_PASSWORD}@carbontracker.tgvu22h.mongodb.net/?retryWrites=true&w=majority`),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
