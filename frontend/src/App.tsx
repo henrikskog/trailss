b43d54b85c2c0df6579978587a51bdaee28ec84a
Cumlord <joseprribera@gmail.com>
2022-10-19 17:44:05 +0200

    react selector needs to be implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import VehiclePicker from './components/shared/vehicle-picker/VehiclePicker';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p><VehiclePicker></VehiclePicker></p>
       </header>
     </div>
   );
---
+++ frontend/src/components/shared/vehicle-picker/VehiclePicker.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Select from 'react-select';
+import './VehiclePicker.scss';
+
+export default function VehiclePicker() {
+  // const options = [
+  //   { value: 'fiesta', label: 'Ford Fiesta' },
+  //   { value: 'astra', label: 'Opel Astra' },
+  //   { value: 'megane', label: 'Renault Megane' }
+  //   ]
+  return (
+    <div className="vehicle-picker">
+        {/* <Select options={options} /> */}
+    </div>
+  );
+}
+
