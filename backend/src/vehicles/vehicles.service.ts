f9dd1879f38cc58729ab6b2d90c2b8df7d38d64d
OSCAR FERNÁNDEZ MARTÍNEZ <osfermar@upv.edu.es>
2022-10-25 19:24:52 +0200

    CRUD finished

--- backend/src/vehicles/vehicles.service.ts
+++ backend/src/vehicles/vehicles.service.ts
@@ -27,7 +27,8 @@ export class VehiclesService {
   update(vehicles: any, id: string, updateVehicleDto: UpdateVehicleDto) {
     const vehicle = vehicles.filter(vehicle => vehicle._id.toString() == id)
 
-    if (!vehicle) throw new NotFoundException("No car with the given id was found")
+    if (!vehicle) throw new NotFoundException("No car with the given id was found");
+    
     this.vehiclesModel.findByIdAndUpdate(updateVehicleDto.id, updateVehicleDto)
     return "Vehicle updated successfully"
   }
