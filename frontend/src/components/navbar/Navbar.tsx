97de35e844cd49b1b70da8a30a637e810adfdc2f
Victor Martinez <vicroc.vn@gmail.com>
2022-10-26 00:40:05 +0200

    removed hamburguer files

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -2,14 +2,12 @@ import { Link } from "react-router-dom";
 import logo from '../../logo.svg';
 import React, { useState } from 'react';
 import "./Navbar.scss";
-import LandingHamburger from "../landing/hamburger/LandingHamburger";
 
 
 
 export default function Navbar() {
     return (
         <header className="navbar">
-            <LandingHamburger />
             <Link to="/"><img src={logo} className="logo" alt="Logo" /></Link>
             <ul id='nav-list'>
                 <li><Link to="map">Map</Link></li>
