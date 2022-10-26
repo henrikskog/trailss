8c576a9ff9ac7baf5f6f6b063291b65db34dd2f8
Victor Martinez <vicroc.vn@gmail.com>
2022-10-26 12:31:45 +0200

    resize globe and midpage landing

--- frontend/src/components/landing/components/BottomPage.tsx
+++ frontend/src/components/landing/components/BottomPage.tsx
@@ -4,7 +4,20 @@ import './BottomPage.scss';
 export default function BottomPage() {
   return (
     <div className="bottompage page">
-       
+       <div className='landing-bottom-title'>
+                <div>Our Projects</div>
+            </div>
+            <div className='landing-bottom-cards'>
+                {/* <div className='cards'>
+                    <Card title='ZK VOTATION WITH PROOFS' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi saepe nulla sed cum officia reprehenderit, impedit excepturi? Perferendis' />
+                </div>
+                <div className='cards center'>
+                    <Card title='SoulBound certificates' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi saepe nulla sed cum officia Lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi saepe nulla sed cum officia reprehenderit, impedit excepturi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi saepe nulla sed cum officia reprehenderit, impedit excepturi?' />
+                </div>
+                <div className='cards'>
+                    <Card title='ZK VOTATION WITH PROOFS' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi saepe nulla sed cum officia reprehenderit, impedit excepturi? Perferendis' />
+                </div> */}
+            </div>
     </div>
   );
 }
