88a5766ddbdca3b2f5b5ee037d0ae5f794ab7d27
henrikskog <henrikskog01@gmail.com>
2022-12-11 16:32:34 +0100

    fix z-index problems

--- frontend/src/components/company/DashboardCompany.tsx
+++ frontend/src/components/company/DashboardCompany.tsx
@@ -12,55 +12,58 @@ import NotFoundTitle from '../shared/notfound/NotFound';
 import MenuBar from '../user/dashboard/components/menubar/MenuBar';
 
 import {
-    TablerIcon,
-    IconHome2,
-    IconCalendarStats,
-    IconUser,
-    IconLogout,
-    IconSwitchHorizontal,
-    IconCar,
-    IconPlaneDeparture,
-    IconCertificate,
-    IconChartBar,
+  TablerIcon,
+  IconHome2,
+  IconCalendarStats,
+  IconUser,
+  IconLogout,
+  IconSwitchHorizontal,
+  IconCar,
+  IconPlaneDeparture,
+  IconCertificate,
+  IconChartBar,
 } from '@tabler/icons';
 
 export default function DashboardCompany() {
-    const [content, setContent] = useState<string>("Home")
+  const [content, setContent] = useState<string>('Home');
 
-const mockdataCompany = [
+  const mockdataCompany = [
     { icon: IconHome2, label: 'Home' },
     { icon: IconCar, label: 'Fleets' },
     { icon: IconPlaneDeparture, label: 'Business trips' },
     { icon: IconCertificate, label: 'Certificates' },
     { icon: IconChartBar, label: 'Statistics' },
     { icon: IconUser, label: 'Account' },
-];
+  ];
 
-    function renderSwitch(param: string) {
-        switch (param) {
-            case 'Home':
-                return <Home />;
-            case 'Fleets':
-                return <Fleets />;
-            case 'Statistics':
-                return <Statistics />;
-            case 'Business trips':
-                return <BusinessTrips />;
-            case 'Certificates':
-                return <Certificates />;
-            case 'Account':
-                return <UserSettings />;
-            default:
-                return <NotFoundTitle />;
-        }
+  function renderSwitch(param: string) {
+    switch (param) {
+      case 'Home':
+        return <Home />;
+      case 'Fleets':
+        return (
+          <>
+            <Fleets />
+            <Fleets />
+            <Fleets />
+          </>
+        );
+      case 'Statistics':
+        return <Statistics />;
+      case 'Business trips':
+        return <BusinessTrips />;
+      case 'Certificates':
+        return <Certificates />;
+      case 'Account':
+        return <UserSettings />;
+      default:
+        return <NotFoundTitle />;
     }
-    return (
-        <div className="dashboard-user">
-            <MenuBar isCompanyDashboard setContent={setContent} data={mockdataCompany} />
-            <div className="dashboard-user-content">
-                {renderSwitch(content)}
-            </div>
-        </div>
-    );
+  }
+  return (
+    <div className="dashboard-user">
+      <MenuBar isCompanyDashboard setContent={setContent} data={mockdataCompany} />
+      <div className="dashboard-user-content">{renderSwitch(content)}</div>
+    </div>
+  );
 }
-
