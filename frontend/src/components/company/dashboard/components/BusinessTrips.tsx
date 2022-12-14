2acd8fda008feb2669c041f35490ad050f9daa4f
henrikskog <henrikskog01@gmail.com>
2022-12-14 17:18:26 +0100

    some small changes in styling and cleanup in code

--- frontend/src/components/company/dashboard/components/BusinessTrips.tsx
+++ frontend/src/components/company/dashboard/components/BusinessTrips.tsx
@@ -1,135 +1,150 @@
-import Grid from '@mui/material/Grid';
-import ListCard from '../../../shared/cards/ListCard';
-import "./BusinessTrips.scss";
-import FooterDashboard from './FooterDashboard';
-import { Button, Divider, NumberInput, Select, TextInput, Title } from '@mantine/core';
-import { DatePicker } from '@mantine/dates';
-import { useForm } from '@mantine/form';
-import { useState } from "react";
+import { Button, Select, TextInput } from '@mantine/core';
 import { Close } from '@mui/icons-material';
-import { SemanticClassificationFormat } from 'typescript';
+import Grid from '@mui/material/Grid';
+import { useState } from 'react';
+import ListCard from '../../../shared/cards/ListCard';
+import './BusinessTrips.scss';
+import FooterDashboard from './FooterDashboard';
 
-
-function onSubmit() {
-    console.log("lol")
+interface Trip {
+  name: string;
+  origin: string;
+  destination: string;
 }
 
 export default function BusinessTrips() {
-    const [cars, setCars] = useState<string[]>([]);
-    const [value, setValue] = useState("");
+  const [cars, setCars] = useState<string[]>([]);
+  const [value, setValue] = useState('');
+  const [showForm, setShowForm] = useState(false);
 
-    const [businessTrips, setBusinessTrips] = useState<any[]>([
-        {
-            id: "1",
-            name: "Meeting",
-            origin: "Valencia",
-            destination: "Cullera"
-        },
-        {
-            id: "1",
-            name: "Dinner",
-            origin: "Valencia",
-            destination: "Xativa"
-        },
-        {
-            id: "1",
-            name: "Project",
-            origin: "Valencia",
-            destination: "Madrid"
-        },
-    ]);
+  const [occasion, setOccasion] = useState<string>('');
+  const [origin, setOrigin] = useState<string>('');
+  const [destination, setDestination] = useState<string>('');
 
-    function handleRemoveCar(index: number) {
-        const updatedCars = [...cars];
-        updatedCars.splice(index, 1);
-        setCars(updatedCars);
-        setValue("")
-    }    
+  const [businessTrips, setBusinessTrips] = useState<Trip[]>([
+    {
+      name: 'Meeting',
+      origin: 'Valencia',
+      destination: 'Cullera',
+    },
+    {
+      name: 'Dinner',
+      origin: 'Valencia',
+      destination: 'Xativa',
+    },
+    {
+      name: 'Project',
+      origin: 'Valencia',
+      destination: 'Madrid',
+    },
+  ]);
 
-    const [form, showForm] = useState(false);
+  function handleRemoveCar(index: number) {
+    const updatedCars = [...cars];
+    updatedCars.splice(index, 1);
+    setCars(updatedCars);
+    setValue('');
+  }
 
-    const handleAddTrip = () => {
-        // get the form data
+  const handleAddTrip = () => {
 
-        const newTrip = {
-            id: 1 as number,
-            name: occasion,
-            origin: origin,
-            destination: destination
-        }
-        // do something with the form data
-        setBusinessTrips([...businessTrips, newTrip])
-        showForm(!form)
-        setCars([])
+    const newTrip = {
+      name: occasion,
+      origin: origin,
+      destination: destination,
     };
 
-    const [occasion, setOccasion] = useState<string>();
-    const [origin, setOrigin] = useState<string>();
-    const [destination, setDestination] = useState<string>();
+    console.log(newTrip)
 
-    return (
-        <div className='company-home-wrapper'>
-            <Grid container rowSpacing={4} columnSpacing={{ xs: 1.5, sm: 2, md: 3 }} justifyContent="space-evenly" alignItems={"start"}>
-                <Grid item xs={12}>
-                    <h1>Business Trips</h1>
-                </Grid>
-                <Grid item xs={12} md={7}>
-                    <div className='box business-trip-box'>
-                        {form ?
-                            <form className="form">
-                                <h2>Create New Business Trip</h2>
-                                <div>{cars.map((car, index) => (
-                                    <span key={index} className="car-element">
-                                        {car}
-                                        <span className='remove-button' onClick={() => handleRemoveCar(index)}>
-                                            <Close className='close-icon' />
-                                        </span>
-                                    </span>
-                                ))}</div>
-                                <Select placeholder="Add car"
-                                    value={value}
-                                    onChange={(event) => {
-                                        setCars(
-                                            [...cars, event!]
-                                        )
-                                        setValue("")
-                                    }}
-                                    data={["Audi A4 2015", "BMW M440i 2016", "Ford Explorer 2019"]} />
-                                <TextInput
-                                    label="Occasion:"
-                                    placeholder="E.g. Meeting"
-                                    value = {occasion}
-                                    onChange={(evt) => setOccasion(evt.currentTarget.value)}
-                                />
-                                <TextInput
-                                    label="Origin:"
-                                    placeholder="E.g. Valencia"
-                                    value = {origin}
-                                    onChange={(evt) => setOrigin(evt.currentTarget.value)}
-                                />
-                                <TextInput
-                                    label="Destination:"
-                                    placeholder="E.g. Madrid"
-                                    value = {destination}
-                                    onChange={(evt) => setDestination(evt.currentTarget.value)}
-                                />
+    // do something with the form data
+    setBusinessTrips([...businessTrips, newTrip]);
+    setShowForm(!showForm);
+    setOrigin('')
+    setOccasion('');
+    setDestination('');
+    setCars([]);
+  };
 
-                            </form>
-                            :
-                            <ListCard elements={businessTrips.slice(0, 5)} ></ListCard>
-                        }
-                        <br></br>
-                        <br></br>
-                        {form && cars.length > 0 ? <Button type="button" onClick={handleAddTrip}>Add Business Trip</Button> : <></>}
-                        <Button className='business-trip-button' onClick={() => showForm(!form)}>{form ? "Cancel" : "Add New Business Trip"}</Button>
+  return (
+    <div className="company-home-wrapper">
+      <Grid
+        container
+        rowSpacing={4}
+        columnSpacing={{ xs: 1.5, sm: 2, md: 3 }}
+        justifyContent="space-evenly"
+        alignItems={'start'}
+      >
+        <Grid item xs={12}>
+          <h1>Business Trips</h1>
+        </Grid>
+        <Grid item xs={12} md={7}>
+          <div className="box business-trip-box">
+            {showForm ? (
+              <form className="form">
+                <h2>Create New Business Trip</h2>
+                <TextInput
+                  label="Occasion:"
+                  className="business-trip-input"
+                  placeholder="E.g. Meeting"
+                  value={occasion}
+                  onChange={(evt) => setOccasion(evt.currentTarget.value)}
+                />
+                <TextInput
+                  label="Origin:"
+                  placeholder="E.g. Valencia"
+                  className="business-trip-input"
+                  value={origin}
+                  onChange={(evt) => setOrigin(evt.currentTarget.value)}
+                />
+                <TextInput
+                  label="Destination:"
+                  className="business-trip-input"
+                  placeholder="E.g. Madrid"
+                  value={destination}
+                  onChange={(evt) => setDestination(evt.currentTarget.value)}
+                />
+                <Select
+                  label="Cars:"
+                  className="business-trip-input"
+                  placeholder="Choose cars"
+                  value={value}
+                  onChange={(event) => {
+                    setCars([...cars, event!]);
+                    setValue('');
+                  }}
+                  data={['Audi A4 2015', 'BMW M440i 2016', 'Ford Explorer 2019']}
+                />
+                <div>
+                  {cars.map((car, index) => (
+                    <div key={index} className="car-element">
+                      {car}
+                      <div className="remove-button" onClick={() => handleRemoveCar(index)}>
+                        <Close className="close-icon" />
+                      </div>
                     </div>
-
-                </Grid>
-
-            </Grid>
-            <FooterDashboard />
-
-        </div>
-
-    );
+                  ))}
+                </div>
+                <div className="trip-form-buttons">
+                  <Button type="button" className="add-trip-button" onClick={handleAddTrip}>
+                    Add Business Trip
+                  </Button>
+                  <Button className="cancel-trip-button" onClick={() => setShowForm(!showForm)}>
+                    Cancel
+                  </Button>
+                </div>
+              </form>
+            ) : (
+              <>
+                <ListCard elements={businessTrips} />
+                <Button type="button" onClick={() => setShowForm(true)}>
+                  Add Business Trip
+                </Button>
+              </>
+            )}
+          </div>
+        </Grid>
+      </Grid>
+      <FooterDashboard />
+    </div>
+  );
 }
--- frontend/src/components/shared/cards/ListCard.tsx
+++ frontend/src/components/shared/cards/ListCard.tsx
@@ -1,4 +1,4 @@
-import { IconCar } from '@tabler/icons';   
+import { IconCar } from '@tabler/icons';
 import { styled } from '@mui/material/styles';
 import List from '@mui/material/List';
 import Grid from '@mui/material/Grid';
@@ -10,100 +10,88 @@ import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
 //import css
 import './ListCard.scss';
 
-
-const listStyle = {
-    backgroundColor: '#f8f8f8',
-    padding: '20px',
-};
-
-const listItemStyle = {
-    fontSize: '16px',
-    fontWeight: 'bold',
-    marginBottom: '8px',
-};
-
 export interface Props {
-    elements: Element[]
+  elements: Element[];
 }
 
 interface Element {
-    id?: string;
-    name: string;
-    quantity?: string;
-    quantity2?: string;
-    origin?: string;
-    destination?: string;
+  id?: string;
+  name: string;
+  quantity?: string;
+  quantity2?: string;
+  origin?: string;
+  destination?: string;
 }
 
-
-
-const ListCard: React.FC<Props> = ({ elements}) => {
-    return (
-        <div className='list-card-wrapper'>
-                <div className='list-card-rows'>
-                    {elements.map((element, i, elements) => {
-                        return (
-                            <ListRows
-                                id={element.id}
-                                name={element.name}
-                                quantity={element.quantity}
-                                quantity2={element.quantity2}
-                                origin={element.origin}
-                                destination={element.destination}
-                                lastOne={i === elements.length - 1}
-                                key={element.id}
-                            />
-                        );
-                    })}
-                </div>
-                <div className='view-all-button'>
-                    <Button size="small" disableElevation>
-                        View All
-                        <ChevronRightOutlinedIcon />
-                    </Button>
-                </div>
-        </div>
-    );
-}
+const ListCard: React.FC<Props> = ({ elements }) => {
+  return (
+    <div className="list-card-wrapper">
+      <div className="list-card-rows">
+        {elements.map((element, i, elements) => {
+          return (
+            <ListRows
+              id={element.id}
+              name={element.name}
+              quantity={element.quantity}
+              quantity2={element.quantity2}
+              origin={element.origin}
+              destination={element.destination}
+              lastOne={i === elements.length - 1}
+              key={i}
+            />
+          );
+        })}
+      </div>
+      <div className="view-all-button">
+        <Button size="small" disableElevation>
+          View All
+          <ChevronRightOutlinedIcon />
+        </Button>
+      </div>
+    </div>
+  );
+};
 export default ListCard;
 
 interface RowData extends Element {
-    lastOne: Boolean
+  lastOne: Boolean;
 }
 
 const ListRows: React.FC<RowData> = (element) => {
-    return (
-        <div className='list-card-row'>
-            <Grid item xs={12}>
-                <Grid container direction="column">
-                    <Grid item>
-                        <Grid container alignItems="center" justifyContent="space-between">
-                            <Grid item>
-                                <Typography variant="subtitle1" color="inherit">
-                                    {element.name}
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <Grid container alignItems="center" justifyContent="space-between">
-                                    <Grid item>
-                                        <Typography className="list-card-row-quantity" variant="subtitle1" color="inherit">
-                                            {element?.quantity ? element.quantity : element.destination}
-                                        </Typography>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Grid item>
-                        <Typography className="list-card-row-quantity2" variant="subtitle2" >
-                            {element?.quantity2 ? element?.quantity2 : "Origin: " + element.origin}
-                        </Typography>
-                    </Grid>
+  return (
+    <div className="list-card-row">
+      <Grid item xs={12}>
+        <Grid container direction="column">
+          <Grid item>
+            <Grid container alignItems="center" justifyContent="space-between">
+              <Grid item>
+                <Typography variant="subtitle1" color="inherit">
+                  {element.name}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Grid container alignItems="center" justifyContent="space-between">
+                  <Grid item>
+                    <Typography
+                      className="list-card-row-quantity"
+                      variant="subtitle1"
+                      color="inherit"
+                    >
+                      {element?.quantity ? element.quantity : element.destination}
+                    </Typography>
+                  </Grid>
                 </Grid>
+              </Grid>
             </Grid>
-            {
-                !element.lastOne && <Divider sx={{ my: 1.5 }} />
-            }
-        </div>
-    );
-}
+          </Grid>
+          <Grid item>
+            <Typography className="list-card-row-quantity2" variant="subtitle2">
+              {element?.quantity2 ? element?.quantity2 : 'Origin: ' + element.origin}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      {!element.lastOne && <Divider sx={{ my: 1.5 }} />}
+    </div>
+  );
+};
